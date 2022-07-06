49101d209e0392daf546db9bd84098c6fdbe9fdb
Khairul Aqram <khairulaqram21@gmail.com>
2022-07-06 20:35:18 +0700

    add home page design

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/sejarah', function () {
+    return view('sejarah');
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
